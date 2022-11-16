067723bbf7cac9ac4bbd147fe812ec471453db6d
Stephen <sdittawork@gmail.com>
2022-11-16 14:14:20 -0500

    fixed redirect routes not found

--- ros-app/src/App.js
+++ ros-app/src/App.js
@@ -16,6 +16,7 @@ function App() {
         <Route path="/" element={<div>Home!</div>} />
         <Route path="/signup" element={<CreateUser />} />
         <Route path="/welcome" element={<Welcome />} />
+        <Route path="*" element={<div>page not found!</div>}></Route>
       </Routes>
     </>
   );
