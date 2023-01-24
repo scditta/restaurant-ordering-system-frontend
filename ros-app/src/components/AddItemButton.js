8e950fe0866e33a28fe080c3f7e55bdb098e7236
paulkim26 <paulkim26.pk@gmail.com>
2023-01-24 12:06:33 -0500

    Image upload function

--- ros-app/src/components/AddItemButton.js
+++ ros-app/src/components/AddItemButton.js
@@ -1,5 +1,15 @@
 import { useState, useContext, useCallback } from 'react';
-import { Modal, Button, Form, InputGroup, Row, Col, Container, Alert } from 'react-bootstrap';
+import {
+  Modal,
+  Button,
+  Form,
+  InputGroup,
+  Row,
+  Col,
+  Container,
+  Alert,
+  Image,
+} from 'react-bootstrap';
 import { useDropzone } from 'react-dropzone';
 
 import MenuContext from '../context/MenuContext';
@@ -29,6 +39,8 @@ export default function AddItemButton(props) {
     price: PRICE_MIN,
     image: '',
   });
+
+  const [imgSrc, setImgSrc] = useState('');
 
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
@@ -60,12 +72,22 @@ export default function AddItemButton(props) {
   };
 
   const onDrop = useCallback((acceptedFiles) => {
-    /*
-    acceptedFiles.map((file) => {
-      //Handle accepted file
+    acceptedFiles.forEach((file) => {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        const data = reader.result.split(',');
+        const imgHeaders = data[0] + ',';
+        const imgBinary = data[1];
+
+        setImgSrc(imgHeaders + imgBinary);
+      };
+      reader.readAsDataURL(file);
     });
-    */
   }, []);
+
+  const clearImage = () => {
+    setImgSrc(null);
+  };
 
   function imageSizeValidator(file) {
     if (file.name.length > IMAGE_FNAME_MAX) {
@@ -129,58 +151,106 @@ export default function AddItemButton(props) {
                 <Col lg={true}>
                   <Form.Group className="mb-3">
                     <Form.Label>Image</Form.Label>
-                    <div style={{ margin: '2em' }}>
-                      <div
-                        className="d-flex align-items-center justify-content-center"
-                        style={{
-                          backgroundColor: 'rgb(233 233 233)',
-                          border: '3px dashed',
-                          marginBottom: '0.5em',
-                          aspectRatio: '1/1',
-                          color: 'rgb(122 122 122)',
-                          borderRadius: '0.5em',
-                          cursor: 'pointer',
-                          textAlign: 'center',
-                        }}
-                        {...getRootProps()}
-                      >
-                        <input {...getInputProps()} />
-                        <div>
-                          {isDragActive ? (
-                            'Drop file here...'
-                          ) : (
-                            <>
-                              Drag and drop image here<br></br>(
-                              {Object.values(IMAGE_ACCEPTED_FILETYPES).map((filetype, i, arr) => (
-                                <span key={filetype[0]}>
-                                  {filetype[0]}
-                                  {i < arr.length - 1 && ' '}
-                                </span>
-                              ))}
-                              )
-                            </>
-                          )}
-                        </div>
-                      </div>
 
-                      <span className="text-muted">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="16"
-                          height="16"
-                          fill="currentColor"
-                          className="bi bi-info-circle"
-                          viewBox="0 0 16 16"
-                          style={{
-                            marginRight: '0.2em',
-                            transform: 'translate(0px, -2px)',
-                          }}
-                        >
-                          <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
-                          <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z" />
-                        </svg>
-                        Image size cannot exceed {IMAGE_MAX_MB}MB.
-                      </span>
+                    <div style={{ position: 'relative', margin: '2em' }}>
+                      {imgSrc ? (
+                        <>
+                          <Image src={imgSrc} fluid thumbnail></Image>
+
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="48"
+                            height="48"
+                            fill="currentColor"
+                            class="bi bi-circle-fill"
+                            viewBox="0 0 16 16"
+                            style={{
+                              position: 'absolute',
+                              top: '1em',
+                              right: '1em',
+                              cursor: 'pointer',
+                              color: 'white',
+                            }}
+                          >
+                            <circle cx="8" cy="8" r="8" />
+                          </svg>
+
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="48"
+                            height="48"
+                            fill="currentColor"
+                            className="bi bi-x-circle"
+                            viewBox="0 0 16 16"
+                            style={{
+                              position: 'absolute',
+                              top: '1em',
+                              right: '1em',
+                              cursor: 'pointer',
+                            }}
+                            onClick={clearImage}
+                          >
+                            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                            <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+                          </svg>
+                        </>
+                      ) : (
+                        <>
+                          <div
+                            className="d-flex align-items-center justify-content-center"
+                            style={{
+                              backgroundColor: 'rgb(233 233 233)',
+                              border: '3px dashed',
+                              marginBottom: '0.5em',
+                              aspectRatio: '1/1',
+                              color: 'rgb(122 122 122)',
+                              borderRadius: '0.5em',
+                              cursor: 'pointer',
+                              textAlign: 'center',
+                            }}
+                            {...getRootProps()}
+                          >
+                            <input {...getInputProps()} />
+                            <div>
+                              {isDragActive ? (
+                                'Drop file here...'
+                              ) : (
+                                <>
+                                  Drag and drop image here<br></br>(
+                                  {Object.values(IMAGE_ACCEPTED_FILETYPES).map(
+                                    (filetype, i, arr) => (
+                                      <span key={filetype[0]}>
+                                        {filetype[0]}
+                                        {i < arr.length - 1 ? ' ' : null}
+                                      </span>
+                                    )
+                                  )}
+                                  )
+                                </>
+                              )}
+                            </div>
+                          </div>
+
+                          <span className="text-muted">
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              width="16"
+                              height="16"
+                              fill="currentColor"
+                              className="bi bi-info-circle"
+                              viewBox="0 0 16 16"
+                              style={{
+                                marginRight: '0.2em',
+                                transform: 'translate(0px, -2px)',
+                              }}
+                            >
+                              <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                              <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z" />
+                            </svg>
+                            Image size cannot exceed {IMAGE_MAX_MB}MB.
+                          </span>
+                        </>
+                      )}
                     </div>
                   </Form.Group>
                 </Col>
