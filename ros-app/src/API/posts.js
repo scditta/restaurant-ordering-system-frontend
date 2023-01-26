1c630d73faa66b058d06b5708a8c82dbd2efd44d
Stephen <sdittawork@gmail.com>
2023-01-25 21:02:18 -0500

    updated axios backend_url environment variable

--- ros-app/src/API/posts.js
+++ ros-app/src/API/posts.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 // require('dotenv').config();
 
 export default axios.create({
-  baseURL: 'https://restaurant-order-system.cyclic.app',
+  baseURL: process.env.REACT_APP_BACKEND_URL,
   headers: {
     // 'X-Requested-With': 'XMLHttpRequest',
     api_key: process.env.REACT_APP_API_KEY,
