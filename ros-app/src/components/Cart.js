1bc233816692c21ba2cf81e1b5ee28f686db74f8
paulkim26 <paulkim26.pk@gmail.com>
2023-02-08 00:50:28 -0500

    Add cart item mock-up

--- ros-app/src/components/Cart.js
+++ ros-app/src/components/Cart.js
@@ -1,16 +1,35 @@
 import { Card, Button } from 'react-bootstrap';
+import { XCircleFill } from 'react-bootstrap-icons';
 
 export default function Cart() {
   return (
     <Card>
       <Card.Body>
         <Card.Title>Cart</Card.Title>
+        <div>
+          <Card>
+            <Card.Body>
+              <span>Vanilla Ice Cream</span>
+              <span className="float-end">
+                Qty: 1 <XCircleFill size={24} style={{ marginLeft: '1em' }}></XCircleFill>
+              </span>
+            </Card.Body>
+          </Card>
+        </div>
         <Card.Text>
-          Subtotal:<span className="float-end">$0.00</span>
-          <br />
-          Tax:<span className="float-end">$0.00</span>
-          <hr></hr>
-          Total:<span className="float-end">$0.00</span>
+          <div>
+            Subtotal:
+            <span className="float-end">$0.00</span>
+          </div>
+          <div>
+            Tax:
+            <span className="float-end">$0.00</span>
+          </div>
+          <hr className="mt-2 mb-2"></hr>
+          <div>
+            Total:
+            <span className="float-end">$0.00</span>
+          </div>
         </Card.Text>
         <div className="d-grid gap-2">
           <Button>Checkout</Button>
