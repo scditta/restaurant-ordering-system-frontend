5b3b7929b0582379fd4ba719c04b4df9fc613e6a
marktmarino76@gmail.com <Markmarino76@gmail.com>
2023-02-08 01:49:41 -0500

    Moved MenuProvider method to app.js as the dashboard component is now irrelevant and can be deleted

--- ros-app/src/App.js
+++ ros-app/src/App.js
@@ -4,19 +4,37 @@ import './App.css';
 
 import CreateUser from './components/CreateUser';
 import Login from './components/Login';
-import DashBoard from './components/DashBoard';
+import OrderGrid from './components/OrderGrid';
+import Menu from './components/Menu';
+import NavBar from './components/NavBar';
 import { AuthProvider } from './context/AuthenticationContext';
-
+import { OrderGridProvider } from '../src/context/OrderGridContext';
+import { MenuProvider } from '../src/context/MenuContext';
 function App() {
   return (
     <>
       <AuthProvider>
-        <DashBoard />
         {/* Routes */}
+        <NavBar />
         <Routes>
-          <Route path="/" />
+          <Route
+            path="/"
+            element={
+              <MenuProvider>
+                <Menu />
+              </MenuProvider>
+            }
+          />
           <Route path="/signup" element={<CreateUser />} />
           <Route path="/login" element={<Login />} />
+          <Route
+            path="/ordergrid"
+            element={
+              <OrderGridProvider>
+                <OrderGrid />
+              </OrderGridProvider>
+            }
+          />
           {/* <Route path="*" element={<div>page not found!</div>} /> */}
         </Routes>
       </AuthProvider>
