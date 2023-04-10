044a0690eb509c68527a58d89904e8a3454cb34b
Stephen <sdittawork@gmail.com>
2023-04-10 03:26:46 -0400

    fixed merge issue for order route

--- ros-app/src/App.js
+++ ros-app/src/App.js
@@ -12,6 +12,7 @@ import DashBoard from './components/DashBoard';
 import OrderHistory from './components/OrderHistory';
 import AuthenticationContext from './context/AuthenticationContext';
 import ChangeCoupon from './components/CouponComponents/ChangeCoupon';
+import YourOrders from './components/YourOrders';
 
 function App() {
   const authUser = useContext(AuthenticationContext);
@@ -50,6 +51,8 @@ function App() {
             </ProtectedRoute>
           }
         />
+        <Route path="/orders" element={<YourOrders />} />
+
         <Route
           path="/couponManagement"
           element={
